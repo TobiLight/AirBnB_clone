9dea5301dab0b1541471620e70b70df10b43b87a
TobiLight <jgetitdonefast@gmail.com>
2023-08-16 06:01:31 +0100

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -36,7 +36,7 @@ class BaseModel():
         Updates the public instance attribute updated_at with the current
         datetime
         """
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
@@ -44,8 +44,10 @@ class BaseModel():
         Returns a dictionary containing all keys/values of __dict__
         of the instance
         """
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict = {}
+        obj_dict.update(self.__dict__)
+        obj_dict.update({'__class__':
+                          (str(type(self)).split('.')[-1]).split('\'')[0]})
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
