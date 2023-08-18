19c22494afa179c50eb6c98c941bcd8031a9ff22
TobiLight <jgetitdonefast@gmail.com>
2023-08-18 12:09:00 +0100

    Fix: code correction

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,7 @@ Defines a base model class BaseModel
 """
 import uuid
 from datetime import datetime
-import models
+from models import storage
 
 
 class BaseModel():
@@ -22,7 +22,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+            storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -37,7 +37,7 @@ class BaseModel():
         datetime
         """
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
         """
