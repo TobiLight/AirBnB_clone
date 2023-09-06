e7b6f7238426192aed1c819173374cb9da4cf24a
TobiLight <jgetitdonefast@gmail.com>
2023-09-06 22:44:48 +0100

    Modified basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -18,20 +18,37 @@ class BaseModel():
         """
         Instantiate an instance of BaseModel
         """
+        # if kwargs is not None and kwargs != {}:
+        #     for key in kwargs:
+        #         if key == "created_at":
+        #             self.__dict__["created_at"] = datetime.strptime(
+        #                 kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+        #         elif key == "updated_at":
+        #             self.__dict__["updated_at"] = datetime.strptime(
+        #                 kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+        #         else:
+        #             self.__dict__[key] = kwargs[key]
+        # else:
+        #     self.id = str(uuid.uuid4())
+        #     self.created_at = datetime.now()
+        #     self.updated_at = datetime.now()
+        #     storage.new(self)
         if kwargs is not None and kwargs != {}:
-            for key in kwargs:
+            for key, value in kwargs.items():
                 if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(
-                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                    self.__dict__["created_at"] = datetime.\
+                        strptime(kwargs["created_at"],
+                                 "%Y-%m-%dT%H:%M:%S.%f")
                 elif key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(
-                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                    self.__dict__["updated_at"] = datetime.\
+                        strptime(kwargs["updated_at"],
+                                 "%Y-%m-%dT%H:%M:%S.%f")
                 else:
-                    self.__dict__[key] = kwargs[key]
+                    self.__dict__[key] = value
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.updated_at = self.created_at
             storage.new(self)
 
     def save(self):
@@ -48,7 +65,7 @@ class BaseModel():
         of the instance
         """
         obj_dict = self.__dict__.copy()
-        obj_dict["__class__"] = type(self).__name__
+        obj_dict["__class__"] = self.__class__.__name__
         obj_dict["created_at"] = obj_dict["created_at"].isoformat()
         obj_dict["updated_at"] = obj_dict["updated_at"].isoformat()
         return obj_dict
